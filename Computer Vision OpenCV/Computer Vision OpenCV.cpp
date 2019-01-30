2a2060f1bba041b859466060dfe6c51d7d3f8fe0
Ahmad Alloush <ahmad.alloush@windowslive.com>
2019-01-30 19:20:26 +0100

    OpenCV setup

---
+++ Computer Vision OpenCV/Computer Vision OpenCV.cpp
@@ -0,0 +1,20 @@
+// Computer Vision OpenCV.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+/*
+Add OpenCV to Project
+in Project properties:
+C/C++ =>  General => Additional Include Directories( $(OPENCV_DIR)\include).
+Linker => General => Additional Library Directories ($(OPENCV_DIR)\x64\vc15\lib).
+
+Linker => Input => Additional Dependencies, Add
+Input Debug:
+opencv_img_hash400d.lib
+opencv_world400d.lib
+------------------------
+Input Relase:
+opencv_img_hash400.lib
+opencv_world400.lib
+*/
+
+#include "pch.h"
+

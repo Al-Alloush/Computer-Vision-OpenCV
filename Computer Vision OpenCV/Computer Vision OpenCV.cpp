b763834b3a9b70b4a7fff851666839e2eff007e8
Ahmad Alloush <ahmad.alloush@windowslive.com>
2019-01-31 20:47:38 +0100

    Colored Object Tracker Colored Object Tracker using HSV color space range{ Scalar(RGB(250, 100, 100))

--- Computer Vision OpenCV/Computer Vision OpenCV.cpp
+++ Computer Vision OpenCV/Computer Vision OpenCV.cpp
@@ -1,20 +1,83 @@
-// Computer Vision OpenCV.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-/*
-Add OpenCV to Project
-in Project properties:
-C/C++ =>  General => Additional Include Directories( $(OPENCV_DIR)\include).
-Linker => General => Additional Library Directories ($(OPENCV_DIR)\x64\vc15\lib).
-
-Linker => Input => Additional Dependencies, Add
-Input Debug:
-opencv_img_hash400d.lib
-opencv_world400d.lib
-------------------------
-Input Relase:
-opencv_img_hash400.lib
-opencv_world400.lib
-*/
-
 #include "pch.h"
+// COLORSPACE-BASED TRACKER
+
+#include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
+
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	// Create the capture object
+	// 0 -> input arg that specifies it should take the input from the webcam
+	VideoCapture cap(0);
+
+	if (!cap.isOpened())
+	{
+		cerr << "Unable to open the webcam. Exiting!" << endl;
+		return -1;
+	}
+
+	Mat frame, hsvImage, mask, outputImage;
+	char ch;
+
+	// Image size scaling factor for the input frames from the webcam
+	float scalingFactor = 0.90;
+
+	// Iterate until the user presses the Esc key
+	while (true)
+	{
+		// Initialize the output image before each iteration
+		outputImage = Scalar(0, 0, 0);
+
+		// Capture the current frame
+		cap >> frame;
+
+		// Check if 'frame' is empty
+		if (frame.empty())
+			break;
+
+		// Resize the frame
+		resize(frame, frame, Size(), scalingFactor, scalingFactor, INTER_AREA);
+
+		// Convert to HSV colorspace
+		cvtColor(frame, hsvImage, COLOR_BGR2HSV);
+
+		//// Define the range of "blue" color in HSV colorspace
+		//Scalar lowerLimit = Scalar(60, 100, 100);
+		//Scalar upperLimit = Scalar(180, 255, 255);
+		//Or
+		// Define the range of "Red" color in HSV colorspace
+		Scalar lowerLimit = Scalar(150, 50, 50);
+		Scalar upperLimit = Scalar(180, 255, 255);
+		//https://convertingcolors.com/rgb-color-60_100_100.html
+
+
+		// Threshold the HSV image to get only blue color
+		inRange(hsvImage, lowerLimit, upperLimit, mask);
+
+		// Compute bitwise-AND of input image and mask
+		bitwise_and(frame, frame, outputImage, mask = mask);
+
+		// Run median filter on the output to smoothen it
+		medianBlur(outputImage, outputImage, 5);
+
+		// Display the input and output image
+		imshow("Input", frame);
+		imshow("Output", outputImage);
+
+		// Get the keyboard input and check if it's 'Esc'
+		// 30 -> wait for 30 ms
+		// 27 -> ASCII value of 'ESC' key
+		ch = waitKey(30);
+		if (ch == 27) {
+			break;
+		}
+	}
+
+	return 0;
+}
 

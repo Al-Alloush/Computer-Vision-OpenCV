0547086c66984588c66d5e60a9646a2a3e2a103a
Ahmad Alloush <ahmad.alloush@windowslive.com>
2019-02-02 16:09:54 +0100

    Mouse Track

--- Computer Vision OpenCV/Computer Vision OpenCV.cpp
+++ Computer Vision OpenCV/Computer Vision OpenCV.cpp
@@ -1,83 +1,84 @@
 #include "pch.h"
-// COLORSPACE-BASED TRACKER
+#include "opencv2/opencv.hpp"
 
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui.hpp"
-
-#include <iostream>
-
-using namespace cv;
 using namespace std;
+using namespace cv;
 
-int main(int argc, char* argv[])
+Point objectPos;
+
+void onMouse(int, int x, int y, int, void*)
 {
-	// Create the capture object
-	// 0 -> input arg that specifies it should take the input from the webcam
-	VideoCapture cap(0);
-
-	if (!cap.isOpened())
-	{
-		cerr << "Unable to open the webcam. Exiting!" << endl;
-		return -1;
-	}
-
-	Mat frame, hsvImage, mask, outputImage;
-	char ch;
-
-	// Image size scaling factor for the input frames from the webcam
-	float scalingFactor = 0.90;
-
-	// Iterate until the user presses the Esc key
-	while (true)
-	{
-		// Initialize the output image before each iteration
-		outputImage = Scalar(0, 0, 0);
-
-		// Capture the current frame
-		cap >> frame;
-
-		// Check if 'frame' is empty
-		if (frame.empty())
-			break;
-
-		// Resize the frame
-		resize(frame, frame, Size(), scalingFactor, scalingFactor, INTER_AREA);
-
-		// Convert to HSV colorspace
-		cvtColor(frame, hsvImage, COLOR_BGR2HSV);
-
-		//// Define the range of "blue" color in HSV colorspace
-		//Scalar lowerLimit = Scalar(60, 100, 100);
-		//Scalar upperLimit = Scalar(180, 255, 255);
-		//Or
-		// Define the range of "Red" color in HSV colorspace
-		Scalar lowerLimit = Scalar(150, 50, 50);
-		Scalar upperLimit = Scalar(180, 255, 255);
-		//https://convertingcolors.com/rgb-color-60_100_100.html
-
-
-		// Threshold the HSV image to get only blue color
-		inRange(hsvImage, lowerLimit, upperLimit, mask);
-
-		// Compute bitwise-AND of input image and mask
-		bitwise_and(frame, frame, outputImage, mask = mask);
-
-		// Run median filter on the output to smoothen it
-		medianBlur(outputImage, outputImage, 5);
-
-		// Display the input and output image
-		imshow("Input", frame);
-		imshow("Output", outputImage);
-
-		// Get the keyboard input and check if it's 'Esc'
-		// 30 -> wait for 30 ms
-		// 27 -> ASCII value of 'ESC' key
-		ch = waitKey(30);
-		if (ch == 27) {
-			break;
-		}
-	}
-
-	return 0;
+	objectPos.x = x;
+	objectPos.y = y;
 }
 
+int main()
+{
+	KalmanFilter kalman(4, 2);
+
+	Mat_<float> tm(4, 4); // transition matrix
+	tm << 1, 0, 1, 0, // next x = 1x+0y+1x'+0y'
+		  0, 1, 0, 1, // next x'= 0x+1y+0x'+1y'
+		  0, 0, 1, 0, // next y = 0x+0y+1x'+0y
+		  0, 0, 0, 1; // next y'= 0x+0y+0x'+1y'
+	kalman.transitionMatrix = tm;
+	Mat_<float> pos(2, 1);
+	pos.at<float>(0) = 0;
+	pos.at<float>(1) = 0;
+
+	kalman.statePre.at<float>(0) = 0; // init x
+	kalman.statePre.at<float>(1) = 0; // init y
+	kalman.statePre.at<float>(2) = 0; // init x'
+	kalman.statePre.at<float>(3) = 0; // init y'
+
+	setIdentity(kalman.measurementMatrix);
+
+	setIdentity(kalman.processNoiseCov,
+		Scalar::all(0.000001));
+
+	string window = "Canvas";
+	namedWindow(window);
+	setMouseCallback(window, onMouse);
+
+	vector<Point> trackRoute;
+	vector<Point> mouseRoute;
+
+	while (waitKey(10) < 0)
+	{
+		// empty canvas
+		Mat canvas(500, 1000, CV_8UC3, Scalar(255, 255, 255));
+
+		pos(0) = objectPos.x;
+		pos(1) = objectPos.y;
+
+		Mat estimation = kalman.correct(pos);
+
+		Point estPt(estimation.at<float>(0),
+			estimation.at<float>(1));
+
+		trackRoute.push_back(estPt);
+		if (trackRoute.size() > 100)//the lang
+			trackRoute.erase(trackRoute.begin());
+
+		polylines(canvas,
+			trackRoute,
+			false,
+			Scalar(0, 0, 255),//Coller
+			5);
+
+		mouseRoute.push_back(objectPos);
+		if (mouseRoute.size() > 100)//the lang
+			mouseRoute.erase(mouseRoute.begin());
+		polylines(canvas,
+			mouseRoute,
+			false,
+			Scalar(0, 0, 0),//Coller
+			2);
+
+		imshow(window, canvas);
+
+		kalman.predict();
+	}
+	return 0;
+
+}
